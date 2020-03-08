05e73d0d2caf632dc548d238a50e454941cf5d05
barneygale <barney.gale@gmail.com>
2020-03-08 02:25:54 +0000

    bpo-39659: Route calls from pathlib.Path to os.getcwd() via the path accessor

--- Lib/pathlib.py
+++ Lib/pathlib.py
@@ -193,7 +193,7 @@ class _WindowsFlavour(_Flavour):
     def resolve(self, path, strict=False):
         s = str(path)
         if not s:
-            return os.getcwd()
+            return path._accessor.getcwd()
         previous_s = None
         if _getfinalpathname is not None:
             if strict:
@@ -358,7 +358,7 @@ class _PosixFlavour(_Flavour):
             return path
         # NOTE: according to POSIX, getcwd() cannot contain path components
         # which are symlinks.
-        base = '' if path.is_absolute() else os.getcwd()
+        base = '' if path.is_absolute() else accessor.getcwd()
         return _resolve(base, str(path)) or sep
 
     def is_reserved(self, parts):
@@ -463,6 +463,8 @@ class _NormalAccessor(_Accessor):
             return grp.getgrgid(self.stat(path).st_gid).gr_name
         except ImportError:
             raise NotImplementedError("Path.group() is unsupported on this system")
+
+    getcwd = os.getcwd
 
 
 _normal_accessor = _NormalAccessor()
@@ -1125,7 +1127,7 @@ class Path(PurePath):
         """Return a new path pointing to the current working directory
         (as returned by os.getcwd()).
         """
-        return cls(os.getcwd())
+        return cls(cls()._accessor.getcwd())
 
     @classmethod
     def home(cls):
@@ -1194,7 +1196,8 @@ class Path(PurePath):
             return self
         # FIXME this must defer to the specific flavour (and, under Windows,
         # use nt._getfullpathname())
-        obj = self._from_parts([os.getcwd()] + self._parts, init=False)
+        parts = [self._accessor.getcwd()] + self._parts
+        obj = self._from_parts(parts, init=False)
         obj._init(template=self)
         return obj
 
